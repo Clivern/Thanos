b0f0695a3636f6d7a3646ba5b5bee2b9f170a7a9
A. F <hello@clivern.com>
2018-12-14 16:14:11 +0100

    Update Data_Structures.py

--- Data_Structures.py
+++ Data_Structures.py
@@ -1,6 +1,38 @@
 import unittest
 
 
+class Lists():
+    pass
+
+
+class Tuples():
+    pass
+
+
+class Dicts():
+    pass
+
+
+class Strings():
+    pass
+
+
+class Sets():
+    pass
+
+
+class Frozensets():
+    pass
+
+
+class Collections():
+    pass
+
+
+class Heapq():
+    pass
+
+
 class Test(unittest.TestCase):
 
     def bingo(self):
