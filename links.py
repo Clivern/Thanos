51b0503e368ea2d758e4b8cd20cc860ef00f5a80
A. F <hello@clivern.com>
2019-01-18 16:01:37 +0100

    Update links.py

--- links.py
+++ links.py
@@ -4,4 +4,5 @@ http://jelices.blogspot.com/
 https://www.youtube.com/watch?v=bum_19loj9A&list=PLBZBJbE_rGRV8D7XZ08LK6z-4zPoWzu5H
 https://www.youtube.com/channel/UCOf7UPMHBjAavgD0Qw5q5ww/videos
 https://github.com/TheAlgorithms/Python
+https://codesays.com/
 """
