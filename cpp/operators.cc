e26f8f236c8f7af896167357dfcb047349793588
A. F <hello@clivern.com>
2019-01-31 23:24:16 +0100

    Update operators.cc

--- cpp/operators.cc
+++ cpp/operators.cc
@@ -1,8 +1,76 @@
 #include <iostream>
 
-using namespace std;
-
 int main()
 {
-    return 0;
+	int h = 5;
+    h += 5;
+    h++;
+    ++h;
+	std::cout << "h => " << h++ << "\n"; // h => 12
+	std::cout << "h => " << ++h << "\n"; // h => 14
+	
+	int x = 4;
+	
+	if(x == 4){
+        std::cout << "x == 4\n";
+    }else if(x > 4){
+	    std::cout << "x > 4\n";
+	}else if (x < 4){
+	    std::cout << "x < 4\n";
+	}else if (x != 4){
+	    std::cout << "x != 4\n";
+	}
+	
+	bool z = true;
+	bool y = false;
+	
+	if(z && y){
+	    std::cout << "z and y are true\n";
+	}else if(z || y){
+	    std::cout << "z or y are true\n";
+	}
+	
+	bool k = true;
+	k &= true;
+	k == true ? std::cout << "k is true\n" : std::cout << "k is false\n";
+	
+	bool o = false;
+	o |= false;
+	o == true ? std::cout << "o is true\n" : std::cout << "o is false\n";
+	
+    int q = 2;
+    if(q > 4)
+        std::cout << "success";
+        // below statement is outside the if condition
+        std::cout << "Not inside the if condition";
+	
+	for(int r=0; r <= 5; r++){
+	    std::cout << "r = " << r << "\n";
+	}
+	
+    int a, b, c;
+    std::cout << "Enter 3 Number\n";
+    std::cin >> a >> b >> c;
+    if(a > b)
+    {
+        if( a > c)
+        {
+            std::cout << "a is greatest\n";
+        }
+        else 
+        {
+            std::cout << "c is greatest\n";
+        }
+    }
+    else
+    {
+        if( b > c)
+        {
+            std::cout << "b is greatest\n";
+        }
+        else
+        {
+            std::cout << "c is greatest\n";
+        }
+    }
 }
