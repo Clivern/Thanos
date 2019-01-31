b8d466cd174c8146936da0a613b83a77bd526a3f
Clivern <hello@clivern.com>
2019-01-31 20:39:09 +0100

    add cpp stuff

--- cpp/data_types.cc
+++ cpp/data_types.cc
@@ -1,7 +1,23 @@
 #include <iostream>
+
 using namespace std;
 
-int main() {
+int main()
+{
+    // Built-in Datatypes
+    char char_chr = 'A';
+    float float_chr = 3.89;
+    double double_chr = 6e-9;
+    bool bool_chr = true;
+
+    int int_chr = 367;
+    long int int_l_chr = 34667777777666;
+    short int int_s_chr = 34233;
+    signed int int_si_chr = -341;
+    unsigned int int_usi_chr = 324;
+
+    // User-defined or Abstract Datatypes
+
    cout << "Size of char : " << sizeof(char) << endl;
    cout << "Size of int : " << sizeof(int) << endl;
    cout << "Size of short int : " << sizeof(short int) << endl;
@@ -9,6 +25,6 @@ int main() {
    cout << "Size of float : " << sizeof(float) << endl;
    cout << "Size of double : " << sizeof(double) << endl;
    cout << "Size of wchar_t : " << sizeof(wchar_t) << endl;
-   
+
    return 0;
 }
