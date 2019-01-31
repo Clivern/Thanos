1fc656437f01b456ebd9d6977566e6af55f62f30
A. F <hello@clivern.com>
2019-01-31 23:44:11 +0100

    Update functions.cc

--- cpp/functions.cc
+++ cpp/functions.cc
@@ -1,8 +1,39 @@
 #include <iostream>
 
-using namespace std;
+/*
+return-type function-name(parameter1, parameter2, ...)
+{
+    // function-body
+}
+*/
+
+// you can also declare the function but optional
+// Function declaration, is done to tell the compiler about the existence of the function.
+// Function's return type, its name & parameter list is mentioned.
+// Function body is written in its definition.
+int sum (int x, int y);
+
+int sum(int x, int y){
+    return x + y;
+}
+
+void call_by_value(int x){
+    x = x + 6;
+}
+
+void call_by_reference(int *x){
+    *x = *x + 6;
+}
 
 int main()
 {
+    std::cout << "3 + 2 = " << sum(3, 2) << "\n"; // 3 + 2 = 5
+
+    int z = 0;
+    call_by_value(z); // will add 6 to a local variable but z still zero
+    std::cout << "z = " << z << "\n"; // z = 0
+    call_by_reference(&z); // will add 6 and affect z
+    std::cout << "z = " << z << "\n"; // z = 6
+
     return 0;
 }
